7816555acba0fd20642056918eef39e597088fc2
Tobias Klein <kle.tobias@googlemail.com>
2025-03-19 17:08:38 +0100

    refactor(generate_dvc_yaml_core.py): add logging to dvc yaml generation
    Add logging messages to record function entry and exit, and remove the mkdir_ dependency.

--- dependencies/templates/generate_dvc_yaml_core.py
+++ dependencies/templates/generate_dvc_yaml_core.py
@@ -1,19 +1,20 @@
 # dependencies/templates/generate_dvc_yaml_core.py
+import logging
 from typing import List
 import jinja2
+import os
+import time
 
-from dependencies.general.mkdir_if_not_exists import mkdir_
-
+logger = logging.getLogger(__name__)
 
 def generate_dvc_yaml_core(
     stages_list: List,
     search_path: str,
     template_name: str,
     dvc_yaml_file_path: str,
-    experiment_directory_path: str,
 ) -> None:
-    mkdir_(experiment_directory_path)
 
+    logger.info("Entering function 'generate_dvc_yaml_core'")
     loader = jinja2.FileSystemLoader(searchpath=search_path)
     env = jinja2.Environment(loader=loader, autoescape=False)
     template = env.get_template(template_name)
@@ -22,3 +23,6 @@ def generate_dvc_yaml_core(
 
     with open(dvc_yaml_file_path, "w", encoding="utf-8") as f:
         f.write(rendered)
+
+    if os.path.exists(dvc_yaml_file_path):
+        logger.info("Exiting function 'generate_dvc_yaml_core', most recent change: dvc.yaml %s", time.time() - os.path.getmtime(dvc_yaml_file_path))
