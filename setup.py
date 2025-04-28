f28a08d902902ed070062f9c452470ba5b0bdc78
Tobias Klein <kle.tobias@googlemail.com>
2025-04-28 12:49:25 +0200

    chore pyc

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="portfolio_medical_drg_ny_gh",
-    version="0.1.0",
+    name="portfolio_medical_drg_ny_gh_ny_311",
+    version="0.1.1",
     packages=find_packages(include=["dependencies*"]),
 )
