4a8071d9e39bbdd3a599e4f2d34386518e202622
Tobias Klein <kle.tobias@googlemail.com>
2025-04-29 15:49:23 +0200

    fix(outputs.tf): update s3 bucket output references

--- infra/terraform/modules/existing_s3_public/outputs.tf
+++ infra/terraform/modules/existing_s3_public/outputs.tf
@@ -1,3 +1,2 @@
-# infra/terraform/modules/s3_bucket/outputs.tf
-output "bucket_name" { value = aws_s3_bucket.this.bucket }
-output "bucket_arn"  { value = aws_s3_bucket.this.arn }
+output "bucket_name" { value = data.aws_s3_bucket.target.bucket }
+output "bucket_arn"  { value = data.aws_s3_bucket.target.arn }
