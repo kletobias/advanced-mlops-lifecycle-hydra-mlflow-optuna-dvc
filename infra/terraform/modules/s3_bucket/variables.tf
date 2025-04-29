84b658b2733eed44ff910214276b566015dda78b
Tobias Klein <kle.tobias@googlemail.com>
2025-04-29 15:26:54 +0200

    chore(variables.tf): reformat Terraform variable definitions
    Remove redundant prod/terraform.tfvars file and adjust variables.tf format for Terraform readability.

--- infra/terraform/modules/s3_bucket/variables.tf
+++ infra/terraform/modules/s3_bucket/variables.tf
@@ -1,6 +1,23 @@
-# infra/terraform/modules/s3_bucket/variables.tf
-variable "bucket_name"         { type = string }
-variable "public_read_prefixes" { type = list(string) default = [] }
-variable "kms_key_id"          { type = string       default = "" }
-variable "versioning"          { type = bool         default = true }
-variable "tags"                { type = map(string)  default = {} }
+variable "bucket_name" {
+  type = string
+}
+
+variable "public_read_prefixes" {
+  type    = list(string)
+  default = []
+}
+
+variable "kms_key_id" {
+  type    = string
+  default = ""
+}
+
+variable "versioning" {
+  type    = bool
+  default = true
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
