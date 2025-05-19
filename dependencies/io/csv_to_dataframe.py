e5c8b0a7aa1d39c7611a5f8cca4150b13baf46e5
Tobias Klein <kle.tobias@googlemail.com>
2025-05-19 16:25:53 +0200

    fix(csv_to_dataframe.py): update parameter name to input_file_path

--- dependencies/io/csv_to_dataframe.py
+++ dependencies/io/csv_to_dataframe.py
@@ -9,10 +9,10 @@ logger = logging.getLogger(__name__)
 
 
 def csv_to_dataframe(
-    file_path: str,
-    low_memory: bool,
+    input_file_path: str,
+    low_memory: bool = False,
 ) -> pd.DataFrame:
     """Reads a CSV file and returns a pd.DataFrame."""
-    df = pd.read_csv(file_path, low_memory=low_memory)
-    logger.info("Read %s, created df", file_path)
+    df = pd.read_csv(input_file_path, low_memory=low_memory)
+    logger.info("Read %s, created df", input_file_path)
     return df
